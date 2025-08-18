1c1d17142f0902de5ae67d7334ea96a8598b6eb3
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-18 20:15:15 +0300

    Initial Commit

---
+++ ReelRover/ReelRoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelRoverApp.swift
+//  ReelRover
+//
+//  Created by Maxim Golovlev on 18.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReelRoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
